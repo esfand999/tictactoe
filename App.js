7c42dea9844be30eef1e9a077bbb33fdb66cade4
esfand999 <61494697+esfand999@users.noreply.github.com>
2021-04-13 23:27:01 +0500

    Task 2

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>  
+      <View >
+        {/* header */}
+        <View style = {{flexDirection: 'row'}}>
+          <Text style={{ fontSize: 30, marginRight: 30 }}>Player1: <Text style={{ fontSize: 30, fontWeight: 'bold' }}>X</Text></Text>
+          <Text style={{ fontSize: 30, marginLeft: 30 }}>Player2: <Text style={{ fontSize: 30, fontWeight: 'bold' }}>O</Text></Text>
+        </View>
+
+
+        {/* rows */}
+        <View style={{ width: 50, flexDirection: 'row'}}>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+        </View>
+        <View style={{ width: 50, flexDirection: 'row'}}>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+        </View>
+        <View style={{ width: 50, flexDirection: 'row'}}>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+          <Pressable style={styles.btn} onPress="">
+            <Text style={styles.btntext}>X</Text>
+          </Pressable>
+        </View>
+      </View> 
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +61,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn: {
+    width: 100,
+    height: 100,
+    margin: 2,
+    backgroundColor: "lightblue",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btntext: {
+    fontFamily: "Calibri",
+    // fontWeight: "bold",
+    fontSize: 40
+
+  }
 });
