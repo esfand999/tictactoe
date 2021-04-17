27fca9511404af2b0be05489c5fa5ccf3f819350
esfand999 <61494697+esfand999@users.noreply.github.com>
2021-04-17 05:17:40 +0500

    Modal

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+
 import React, { useState } from 'react';
 import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
 
@@ -8,38 +9,87 @@ var board = [["", "", ""], ["", "", ""], ["", "", ""]]
 export default function App() {
 
   const [getTurn, setTurn] = useState(true)
+  const [getWinner, setWinner] = useState("")
   const [modalVisible, setModalVisible] = useState(false);
+
   // const [getboard, setboard] = useState("")
   // const [getDisplay, setDisplay] = useState()
 
-  const win = () => {
-    for (var i = 0; i < 2; i++) {
-      if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != "")
-        alert("lets gooooo")
+
+  const nexTurn = () => {
+    if (getTurn){
+      return("Player 1")
     }
-    for (var j = 0; j < 2; j++) {
-      if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != "")
-        alert("lets gooooo")
+    else{
+      return("Player 2")
     }
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != "")
-      alert("lets gooooo")
-    else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != "")
-      alert("lets gooooo")
-    
   }
 
+  // New Game 
+  const newGame = () => {
+    board = [["", "", ""], ["", "", ""], ["", "", ""]]
+    setWinner("")
+    setModalVisible(false)
+    setTurn(true)
+  }
+
+  
+
+  // Win conditions 
+  const win = (w) => {
+    for (var i = 0; i <= 2; i++) {
+      if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ""){
+        setWinner(w)
+        setModalVisible(true)
+      }
+    }
+    for (var j = 0; j <= 2; j++) {
+      if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ""){
+        setWinner(w)
+        setModalVisible(true)
+      }
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ""){
+      setWinner(w)
+      setModalVisible(true)
+
+    }
+    else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ""){
+      setWinner(w)
+      setModalVisible(true)
+
+    }
+    else{
+      var indexCount = 0
+      for(var x = 0; x <= 2; x++){
+        for(var y = 0; y <= 2; y++){
+          if(board[x][y] != ""){
+            indexCount += 1
+          }
+        }
+      }
+      if (indexCount == 9){
+        alert("draw")
+      }
+    }
+      
+  }
+
+  // Pressable function 
   const ButtonPress = (a, b) => {
     if (board[a][b] == "") {
       if (getTurn) {
         board[a][b] = "❌";
         setTurn(false);
+        win("❌");
+
       }
       else {
         board[a][b] = "⭕";
         setTurn(true);
+        win("⭕");
       }
     }
-    win();
   }
 
 
@@ -47,70 +97,72 @@ export default function App() {
   return (
 
     <View style={styles.container}>
-      <View >
 
+      <View >
         {/* header */}
         <View style={{ flexDirection: 'row' }}>
-          <Text style={{ fontSize: 30, marginRight: 30, fontWeight: "bold", fontSize: 20, marginBottom:20}}>Player 1: <Text style={{ fontSize: 30, fontWeight: 'bold' }}>❌</Text></Text>
-          <Text style={{ fontSize: 30, marginLeft: 30, fontWeight: "bold", fontSize: 20, marginBottom:20}}>Player 2: <Text style={{ fontSize: 30, fontWeight: 'bold' }}>⭕</Text></Text>
+          <Text style={{fontSize: 30, marginRight: 30, fontWeight: "bold", fontSize: 20, marginBottom:20}}>Player 1: <Text style={{fontSize: 30, fontWeight: 'bold' }}>❌</Text></Text>
+          <Text style={{fontSize: 30, marginLeft: 30, fontWeight: "bold", fontSize: 20, marginBottom:20}}>Player 2: <Text style={{ ontSize: 30, fontWeight: 'bold' }}>⭕</Text></Text>
+        </View>
+
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={{ fontSize: 30, fontWeight: "bold", fontSize: 20, marginBottom:20}}>Next turn: {nexTurn()}</Text>
         </View>
 
 
         {/* rows */}
         <View>
           <View style={{ width: 50, flexDirection: 'row' }}>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(0, 0) }}>
+            <Pressable style={styles.btn} onPress={() => {ButtonPress(0, 0)}}>
               <Text style={styles.btntext}>{board[0][0]}</Text>
             </Pressable>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(0, 1) }}>
+            <Pressable style={styles.btn} onPress={() => {ButtonPress(0, 1)}}>
               <Text style={styles.btntext}>{board[0][1]}</Text>
             </Pressable>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(0, 2) }}>
+            <Pressable style={styles.btn} onPress={() => {ButtonPress(0, 2)}}>
               <Text style={styles.btntext}>{board[0][2]}</Text>
             </Pressable>
           </View>
 
           <View style={{ width: 50, flexDirection: 'row' }}>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(1, 0) }}>
+            <Pressable style={styles.btn} onPress={() => {ButtonPress(1, 0)}}>
               <Text style={styles.btntext}>{board[1][0]}</Text>
             </Pressable>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(1, 1) }}>
+            <Pressable style={styles.btn} onPress={() => {ButtonPress(1, 1)}}>
               <Text style={styles.btntext}>{board[1][1]}</Text>
             </Pressable>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(1, 2) }}>
+            <Pressable style={styles.btn} onPress={() => { ButtonPress(1, 2)}}>
               <Text style={styles.btntext}>{board[1][2]}</Text>
             </Pressable>
           </View>
 
           <View style={{ width: 50, flexDirection: 'row' }}>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(2, 0) }}>
+            <Pressable style={styles.btn} onPress={() => {ButtonPress(2, 0)}}>
               <Text style={styles.btntext}>{board[2][0]}</Text>
             </Pressable>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(2, 1) }}>
+            <Pressable style={styles.btn} onPress={() => {ButtonPress(2, 1)}}>
               <Text style={styles.btntext}>{board[2][1]}</Text>
             </Pressable>
-            <Pressable style={styles.btn} onPress={() => { ButtonPress(2, 2) }}>
+            <Pressable style={styles.btn} onPress={() => {ButtonPress(2, 2)}}>
               <Text style={styles.btntext}>{board[2][2]}</Text>
             </Pressable>
           </View>
         </View>
       </View>
 
-      {/* <View style={styles.centeredView}>
-        <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => {Alert.alert("Modal has been closed."); setModalVisible (!modalVisible);}}>
+      {/* Modal */}
+      <View style={styles.centeredView}>
+        <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => {setModalVisible (!modalVisible);}}>
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
-              <Text style={styles.modalText}>Hello World!</Text>
-              <Pressable style={[styles.button, styles.buttonClose]} onPress={() => setModalVisible(!modalVisible)}>
-                <Text style={styles.textStyle}>Hide Modal</Text>
+              <Text style={styles.modalText}>Winner: {getWinner}</Text>
+              <Pressable style={[styles.button, styles.buttonClose]} onPress={() => {newGame()}}>
+                <Text style={styles.textStyle}>Play Again</Text>
               </Pressable>
             </View>
           </View>
         </Modal>
-        <Pressable style={[styles.button, styles.buttonOpen]} onPress={() => setModalVisible(true)}>
-          <Text style={styles.textStyle}>Show Modal</Text>
-        </Pressable>
-      </View> */}
+      </View>
     </View>
   );
 }
@@ -133,5 +185,46 @@ const styles = StyleSheet.create({
   btntext: {
     fontFamily: "Calibri",
     fontSize: 40
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
 });
